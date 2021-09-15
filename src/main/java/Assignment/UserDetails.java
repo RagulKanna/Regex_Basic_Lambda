3118d3cd4924ce54cac66f7f02259ae69cc5b4c1
RagulKanna <ragulragi619@gmail.com>
2021-09-15 20:13:44 +0530

    Added:Checking Password is following rules or not

--- src/main/java/Assignment/UserDetails.java
+++ src/main/java/Assignment/UserDetails.java
@@ -44,7 +44,7 @@ public class UserDetails extends Regex {
 
     public static void password() {
         System.out.println("\nEnter the password with following rules");
-        System.out.println("1. minimum 8 characters");
+        System.out.println("1. minimum 8 characters\n2.At Least one uppercase");
         password = scan.nextLine();
         Regex.password(password);
     }
